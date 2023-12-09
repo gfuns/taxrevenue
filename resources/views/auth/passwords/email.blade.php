273ac2c7f8305800cc1f75f752d3150dcce9c406
Gabriel Nwankwo <gfunzy@gmail.com>
2023-12-09 09:53:12 +0100

    I nitial Commit

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,85 +1,115 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="js">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="{{ env('APP_NAME') }}">
-    <meta name="keywords" content="">
-    <meta name="author" content="Gabriel Nwankwo">
-
-
-    <!-- Favicon icon-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
+    <title>Reset Password | {{ env('APP_NAME') }}</title>
+    <link rel="stylesheet" href="{{ asset('auth/assets/css/vendor.bundle.css') }}">
+    <link rel="stylesheet" href="{{ asset('auth/assets/css/style.css') }}">
 
-    <!-- Libs CSS -->
-    <link href="{{ asset('assets/fonts/feather/feather.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/libs/bootstrap-icons/font/bootstrap-icons.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/libs/mdi/font/css/materialdesignicons.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/libs/simplebar/dist/simplebar.min.css') }}" rel="stylesheet">
-    <!-- Theme CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/theme.min.css') }}">
-    <title>{{ env('APP_NAME') }} | Forgot Password</title>
 </head>
 
-<body>
-    <main>
-        <section class="container d-flex flex-column">
-            <div class="row align-items-center justify-content-center g-0 min-vh-100">
-                <div class="col-lg-5 col-md-8 py-8 py-xl-0">
-                    <!-- Card -->
-                    <div class="card shadow">
-                        <!-- Card body -->
-                        <div class="card-body p-6">
-                            <div class="mb-4 row">
-                                <div class="col-md-8 col-12">
-                                    <a href="/admin"><img src="{{ asset('images/logo.png') }}"
-                                            class="mb-4" alt="" style="max-width: 250px"/></a>
-                                </div>
-                                <div class="col-md-8 col-12">
-                                    <h2 class="mb-1 fw-bold">Forgot Password</h2>
-                                    <p>Fill the form to reset your password.</p>
-                                </div>
-                            </div>
-                            <!-- Form -->
-                            <form class="needs-validation" novalidate method="post"
-                                action="{{ route('forgotPassword') }}">
-                                @csrf
+<body class="page-ath theme-modern page-ath-modern">
 
-                                <!-- Email -->
-                                <div class="mb-3">
-                                    <label for="email" class="form-label">Email</label>
-                                    <input type="email" value="{{ old('email') }}" id="email" class="form-control" name="email"
-                                        placeholder="Enter Your Account Email " required />
-                                    <div class="invalid-feedback">Please provide your account email.</div>
-                                    @error('email')
-                                        <span class="" role="alert">
-                                            <strong style="color: #b02a37; font-size:12px">{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <!-- Button -->
-                                <div class="mb-3 d-grid">
-                                    <button type="submit" class="btn btn-primary"> Send Resest Link </button>
-                                </div>
-                                <span>Return to <a href="/admin">sign in</a></span>
-                            </form>
+    <div class="page-ath-wrap flex-row-reverse">
+        <div class="page-ath-content">
+            <div class="page-ath-header">
+                <a href="/" class="page-ath-logo">
+                    <img class="page-ath-logo-img" src="{{ asset('storage/general/logo.png') }}"
+                        alt="{{ env('APP_NAME') }}">
+                </a>
+            </div>
+
+            <div class="page-ath-form">
+
+                <h2 class="page-ath-heading">Reset Password
+                    <span style="font-size: 14px">If you forgot your password, well, then we'll
+                        email you instructions to reset your password.</span>
+                </h2>
+                <form method="POST" action="#" class="validate validate-modern">
+                    @csrf
+                    <div class="input-item">
+                        <input type="email" placeholder="Your Email Address" name="email" value=""
+                            class="input-bordered" required>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div>
+                            <input type="hidden" name="recaptcha" id="recaptcha">
+                            <button type="submit" class="btn btn-primary btn-block">Send Reset Link</button>
                         </div>
+                        <div>
+                            <a href="/login">Return to login</a>
+                        </div>
+                    </div>
+                    <div class="gaps-0-5x"></div>
+                </form>
+
+            </div>
+
+            <div class="page-ath-footer">
+                <ul class="socials mb-3">
+                    <li><a href="#"><em class="fab fa-facebook-f"></em></a></li>
+                    <li><a href="#"><em class="fab fa-twitter"></em></a></li>
+                    <li><a href="#"><em class="fab fa-linkedin-in"></em></a></li>
+                    <li><a href="#"><em class="fab fa-github-alt"></em></a></li>
+                    <li><a href="#"><em class="fab fa-youtube"></em></a></li>
+                    <li><a href="#"><em class="fab fa-medium-m"></em></a></li>
+                    <li><a href="#"><em class="fab fa-telegram-plane"></em></a></li>
+                </ul>
+                <ul class="footer-links guttar-20px align-items-center">
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms and Condition</a></li>
+                    <li>
+                        <div class="lang-switch relative"><a href="javascript:void(0)"
+                                class="lang-switch-btn toggle-tigger">EN<em class="ti ti-angle-up"></em></a>
+                            <div class="toggle-class dropdown-content dropdown-content-up">
+                                <ul class="lang-list">
+                                    <li><a href="?lang=en">English</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <div class="copyright-text">&copy; {{ date('Y') }} {{ env('APP_NAME') }}. All Right Reserved.</div>
+            </div>
+        </div>
+        <div class="page-ath-gfx" style="background-image: url({{ asset('auth/images/ath-gfx.png') }});">
+            <div class="w-100 d-flex justify-content-center">
+                <div class="col-md-11 col-xl-11">
+                    <div style="padding-bottom: 30px">
+                        <a href="/"><span
+                                style="background-color: white; color: #690068; padding:10px; border-radius: 20px"><strong>Back
+                                    to Home</strong></span></a>
+                    </div>
+
+                    <div style="margin-top: 450px; margin-bottom: 50px">
+                        <span style="color:white; font-size: 72px; font-weight:bolder">Welcome to</span>
+                        <span style="color:#FEBA00; font-size: 72px; font-weight:bolder"> &nbsp;Arete</span>
+
+                        <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                            ullamcorper nisl erat, vel convallis elit fermentum pellentesque. Sed mollis velit facilisis
+                            facilisis viverra.</p>
                     </div>
                 </div>
             </div>
-        </section>
-    </main>
-    <!-- Scripts -->
-    <!-- Libs JS -->
-    <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/simplebar/dist/simplebar.min.js') }}"></script>
+        </div>
+    </div>
 
-    <!-- Theme JS -->
-    <script src="{{ asset('assets/js/theme.min.js') }}"></script>
-    @include('sweetalert::alert')
+    <script src="{{ asset('auth/assets/js/jquery.bundle.js') }}"></script>
+    <script src="{{ asset('auth/assets/js/script.js') }}"></script>
+    <script type="text/javascript">
+        jQuery(function() {
+            var $frv = jQuery('.validate');
+            if ($frv.length > 0) {
+                $frv.validate({
+                    errorClass: "input-bordered-error error"
+                });
+            }
+        });
+    </script>
+
 </body>
 
 </html>
