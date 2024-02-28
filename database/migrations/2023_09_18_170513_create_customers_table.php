12f2e8c244c0775999e67ff8e6f7de81a7f7a811
Gabriel Nwankwo <gfunzy@gmail.com>
2024-02-28 09:45:23 +0100

    Critical Fixes

--- database/migrations/2023_09_18_170513_create_customers_table.php
+++ database/migrations/2023_09_18_170513_create_customers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->text('first_name');
             $table->text('last_name');
             $table->text('username')->nullable();
-            $table->string('email');
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone')->nullable();
             $table->string('country')->nullable();
